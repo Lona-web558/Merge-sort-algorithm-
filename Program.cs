f07e0f914b525bf0cb98bba609ae58926176905f
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2024-07-23 12:59:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Program
+{
+    static void Merge(int[] arr, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        Array.Copy(arr, left, leftArray, 0, n1);
+        Array.Copy(arr, middle + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2)
+        {
+            if (leftArray[i] <= rightArray[j])
+            {
+                arr[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void MergeSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = left + (right - left) / 2;
+
+            MergeSort(arr, left, middle);
+            MergeSort(arr, middle + 1, right);
+
+            Merge(arr, left, middle, right);
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 12, 11, 13, 5, 6, 7 };
+
+        Console.WriteLine("Original array:");
+        PrintArray(numbers);
+
+        MergeSort(numbers, 0, numbers.Length - 1);
+
+        Console.WriteLine("Sorted array:");
+        PrintArray(numbers);
+    }
+}
